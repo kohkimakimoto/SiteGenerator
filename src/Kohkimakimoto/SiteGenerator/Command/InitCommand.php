6beab2335034ee8210eb6064562ecc7898dd3d2d
kohkimakimoto <kohki.makimoto@gmail.com>
2014-02-21 13:52:09 +0900

    Update

--- src/Kohkimakimoto/SiteGenerator/Command/InitCommand.php
+++ src/Kohkimakimoto/SiteGenerator/Command/InitCommand.php
@@ -25,13 +25,46 @@ class InitCommand extends Command
         $config = $this->getConfig();
 
         $fs = new Filesystem();
-        $fs->mkdir($config->source, 0755);
-        $fs->mkdir($config->dest, 0755);
-        $fs->mkdir($config->public, 0755);
-        $fs->mkdir($config->views, 0755);
-        $fs->mkdir($config->layouts, 0755);
-        $fs->mkdir($config->includes, 0755);
-        $fs->mkdir($config->helpers, 0755);
+        if (!$fs->exists(getcwd()."/generator.yml")) {
+            $fs->copy(__DIR__."/../Foundation/generator.yml", getcwd()."/generator.yml");
+            $output->writeln("<info>Created:</info> ".getcwd()."/generator.yml");
+        }
+
+        if (!$fs->exists($config->source)) {
+            $fs->mkdir($config->source, 0755);
+            $output->writeln("<info>Created:</info> ".$config->source);
+        }
+
+        if (!$fs->exists($config->public)) {
+            $fs->mkdir($config->public, 0755);
+            $output->writeln("<info>Created:</info> ".$config->public);
+        }
+
+        if (!$fs->exists($config->views)) {
+            $fs->mkdir($config->views, 0755);
+            $output->writeln("<info>Created:</info> ".$config->views);
+        }
+
+        if (!$fs->exists($config->layouts)) {
+            $fs->mkdir($config->layouts, 0755);
+            $output->writeln("<info>Created:</info> ".$config->layouts);
+        }
+
+        if (!$fs->exists($config->includes)) {
+            $fs->mkdir($config->includes, 0755);
+            $output->writeln("<info>Created:</info> ".$config->includes);
+        }
+
+        if (!$fs->exists($config->helpers)) {
+            $fs->mkdir($config->helpers, 0755);
+            $output->writeln("<info>Created:</info> ".$config->helpers);
+        }
+        
+        if (!$fs->exists($config->dest)) {
+            $fs->mkdir($config->dest, 0755);
+            $output->writeln("<info>Created:</info> ".$config->dest);
+        }
+
         $output->writeln("<info>Done.</info>");
     }
 }
